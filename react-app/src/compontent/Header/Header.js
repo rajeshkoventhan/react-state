f19a7a1aa0785f57162f8ebc8933f284cfa6f69d
rajeshkoventhan <93203338+rajeshkoventhan@users.noreply.github.com>
2023-01-10 20:36:50 +0530

    first

--- react-app/src/compontent/Header/Header.js
+++ react-app/src/compontent/Header/Header.js
@@ -1,17 +1,28 @@
-//import { useState,} from "react"
+//import { useEffect, useState,} from "react"
 
 //const Header = () =>{
- //const [number,setnumber] = useState(0)
+//const [number,setnumber] = useState(0)
+//const [point,setpoint] = useState(0)
 
  //const handle =()=>{
   //setnumber (number + 1)  
  //}
 
+ //const sethandle = ()=>{
+ //   setpoint(point - 1)  
+ //}
+
+//useEffect(()=>{
+    //console.log("helow")
+//},[])
+
  //return(
  //<div className="header">
  //<h1>{number}</h1>
 //<button onClick={handle}>click</button>
- //</div>
+//<button onClick={sethandle}>click</button>
+//<h1>{point}</h1>
+// </div>
 //)   
 //}
 //export default Header;
@@ -21,7 +32,7 @@ import  { useReducer } from "react";
 const Reducer = (state,action)=>{
 switch(action.type){
 case "INCREMENT":
-return{count:state.count+1}   
+return{count:state.count+1} 
 case "DECREMENT":
 return{count:state.count-1}   
 default:
@@ -33,13 +44,13 @@ default:
 const DefaulTreduce = ()=>{
  const[result,dispatch] = useReducer(Reducer,{count:0}) 
  
- return(
+return(
     <div>
-        <p>{result.count}</p>
+      <p>{result.count}</p>
         <button onClick={()=>{
             dispatch({type:"INCREMENT"})
         }}>click</button>
-            <button onClick={()=>{
+           <button onClick={()=>{
             dispatch({type:"DECREMENT"})
         }}>click</button>
     </div>
